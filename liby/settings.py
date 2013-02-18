9e40d5d3d72d12d4ee55d60917172fdedd35cc3f
Andraz Brodnik <brodul@brodul.org>
2013-02-18 15:40:34 +0100

    Added flatpages (installed apps) adn flatpages urls

--- liby/settings.py
+++ liby/settings.py
@@ -93,6 +93,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -120,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'django.contrib.flatpages',
     ###########
     ###########
     'liby.books',
--- liby/urls.py
+++ liby/urls.py
@@ -5,16 +5,22 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'liby.books.views.home', name='home'),
+    url(r'^katalog/$', 'liby.books.views.home', name='katalog'),
     # url(r'^liby/', include('liby.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    
+
     # Grappeli
     url(r'^grappelli/', include('grappelli.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('django.contrib.flatpages.views',
+    url(r'^$', 'flatpage', {'url': '/o-nas/'}, name='home'),
+    url(r'^o-nas/$', 'flatpage', {'url': '/o-nas/'}, name='onas'),
+    url(r'^skener/$', 'flatpage', {'url': '/skener/'}, name='skener'),
+    url(r'^license/$', 'flatpage', {'url': '/license/'}, name='license'),
 )
