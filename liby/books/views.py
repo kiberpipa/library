9a06d2e176274096744d83c81613bb90ed0ffc4b
Andraz Brodnik <brodul@brodul.org>
2013-02-23 02:16:21 +0100

    Add flatpage to catalog

--- liby/books/views.py
+++ liby/books/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.flatpages.models import FlatPage
 
 from liby.books.models import Book
 
+
 def home(request):
     books = Book.objects.all()
-    return render(request, "home.html", {"books": books})
+    flatpage = FlatPage.objects.get(url="/katalog/")
+    return render(request, "home.html", {"books": books, "flatpage": flatpage})
