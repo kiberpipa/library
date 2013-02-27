a0370350f282207df1c7d80411663aef5bea3bde
Andraz Brodnik <brodul@brodul.org>
2013-02-27 15:15:29 +0100

    Genre lenght in database to short.

--- liby/books/models.py
+++ liby/books/models.py
@@ -15,7 +15,7 @@ class Genre(models.Model):
 
     For example: drama, IT, horor
     """
-    genre_name = models.CharField(max_length=20, unique=True)
+    genre_name = models.CharField(max_length=100, unique=True)
 
     def __unicode__(self):
             return self.genre_name
