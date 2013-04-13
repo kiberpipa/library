18d81eb2cf85610b571369bdd20a03c0b8dd1122
Matej Cotman <matejc@kiberpipa.org>
2013-04-13 19:02:24 +0200

    some more tests

--- liby/blog/tests.py
+++ liby/blog/tests.py
@@ -1,15 +1,14 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
+This file contains tests using the unittest module. These will pass
 when you run "bin/django test blog".
 """
 
-from django.test import TestCase
-from liby.blog.models import BlogPost
 from django.core.management import call_command
 from django.db.models import loading
+from django.test import TestCase
 from django.test.client import Client
-
-import datetime
+from django.utils.timezone import now
+from liby.blog.models import BlogPost
 
 
 class TestStatusCodes(TestCase):
@@ -27,10 +26,18 @@ class TestStatusCodes(TestCase):
         self.blogpost = BlogPost.objects.create(
             title="Title #1",
             body="Some long text for testing body.",
-            created=datetime.date(2000, 1, 1),
+            created=now(),
         )
 
+        # add tags to blog post
+        self.blogpost.tags.add("tag1", "tag2")
+
         self.client = Client()
+
+    def tearDown(self):
+        """Tear down after each test, flush database and client."""
+        call_command('flush', interactive=False, verbosity=0)
+        del self.client
 
     def test_blog(self):
         """Test if main blog page returns status code 200."""
@@ -45,4 +52,29 @@ class TestStatusCodes(TestCase):
     def test_blog_404(self):
         """Test if n-th blog page returns status code 404."""
         response = self.client.get('/blog/1000')
+        self.assertEqual(response.status_code, 404)
+
+    def test_blog_archives(self):
+        """Test if archives of blogs page returns status code 200."""
+        response = self.client.get('/blog/archives/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_tag_page(self):
+        """Test if tag page returns status code 200."""
+        response = self.client.get('/blog/tag/tag1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_tag_404(self):
+        """Test if nonexistent tag page returns status code 200."""
+        response = self.client.get('/blog/tag/notag')
+        self.assertEqual(response.status_code, 404)
+
+    def test_feed(self):
+        """Test if feed url returns status code 200."""
+        response = self.client.get('/blog/feed/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_404(self):
+        """Test if nonexistent page has status code 404."""
+        response = self.client.get('/blog/nopage/')
         self.assertEqual(response.status_code, 404)
